51604a3b149c5d2f0a6e7c1f31f4269b0015aa2c
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-25 15:29:39 +0300

    задаа решена

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string method(int N)
+{
+    int num1 = N / 10000;
+    int num2 = N / 1000 % 10;
+    int num4 = N / 10 % 10;
+    int num5 = N % 10;
+   
+    if (num1 == num5 && num2 == num4)
+    {
+        return "Да";
+    }
+    else
+    {
+        return "Нет";
+    }
+}
+
+
+int N = 12551;
+string result = method(N);
+Console.WriteLine(result);
