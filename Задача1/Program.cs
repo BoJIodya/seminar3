5d639da21daceb56930558d295eb610be86a5222
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-25 11:33:27 +0300

    решил задачу

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int F(int num1, int num2)
+{
+    if (num1 > 0 && num2 > 0)
+    {
+        return 1;
+    }
+    if (num1 < 0 && num2 > 0)
+    {
+        return 2;
+    }
+    if (num1 < 0 && num2 < 0)
+    {
+        return 3;
+    }
+    else
+    {
+        return 4;
+    }
+}
+
+
+int x = 3;
+int y = -5;
+Console.WriteLine(F(x,y));
