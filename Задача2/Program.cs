550be2286bf938f2ec0ef856c08e8264bcc46edd
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-25 11:58:11 +0300

    program ready

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Method(double Ax, double Ay, double Bx, double By)
+{
+    double lengX = (Bx - Ax);
+    double lengY = (By - Ay);
+    double Axy = Math.Sqrt(lengX * lengX + lengY * lengY);
+    return Axy;
+}
+
+double Ax = 3;
+double Ay = 4;
+double Bx = 7;
+double By = 9;
+
+Double result = Method(Ax, Ay, Bx, By);
+Console.WriteLine(result);
