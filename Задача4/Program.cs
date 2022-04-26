1a9b0347259b348f628141e5193daeaa8d4458c0
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-26 11:55:46 +0300

    корректировка программы

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -4,7 +4,7 @@
     int num2 = N / 1000 % 10;
     int num4 = N / 10 % 10;
     int num5 = N % 10;
-   
+
     if (num1 == num5 && num2 == num4)
     {
         return "Да";
@@ -16,6 +16,13 @@
 }
 
 
-int N = 12551;
-string result = method(N);
-Console.WriteLine(result);
+int N = 1235;
+if (N >= 10000 && N <= 99999)
+{
+    string result = method(N);
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
